fed794faaf1d3e38030d860bd9cd090dcb766b64
Jay Sabva <202101224@daiict.ac.in>
2024-04-13 14:00:02 +0530

    Client Update

--- Client/client.py
+++ Client/client.py
@@ -20,6 +20,29 @@ def write_file(filename):
     else:
         print(f"File {filename} is locked by another client.")
 
+def read_file(filename):
+    proxy = xmlrpc.client.ServerProxy("http://localhost:9000/", allow_none=True)
+    response = proxy.read(filename)
+    print(response)
+    for server in response:
+        addr = server[0]
+        port = server[1]
+
+        proxy1 = xmlrpc.client.ServerProxy(f"http://{addr}:{port}/", allow_none=True)
+        data = proxy1.read(filename)
+        if data != False:
+            print(f"Data in {filename}:")
+            print(data)
+            break
+
+    if len(response) == 0:
+        print(f"File {filename} not found.")
+
+
 if __name__ == "__main__":
-    filename = input("Enter the filename: ")
+    filename = input("Enter the filename to write: ")
     write_file(filename)
+
+#     add functionality for reading the file
+    filename = input("Enter the filename to read: ")
+    read_file(filename)
