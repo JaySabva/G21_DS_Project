1cc8f687ca4523c811fa8bcfcb832cb776ad37f8
Jay Sabva <202101224@daiict.ac.in>
2024-04-13 13:32:59 +0530

    FileServer Update

--- FileServer_B/FileServer_B.py
+++ FileServer_B/FileServer_B.py
@@ -1,55 +1,32 @@
 import os
-import openpyxl
 import xmlrpc.client
 from xmlrpc.server import SimpleXMLRPCServer
+import openpyxl
 from random import choice
-
 
 FileServer_B = SimpleXMLRPCServer(('localhost', 9002), logRequests=True, allow_none=True)
 
 def write(filename, data, primary):
-    file = open(filename, "w")
-    if file:
-        file.write(data)
-        file.close()
-        if primary == True:
-            print("Sending data to backup servers - 1")
-            server_file = "Servers.xlsx"
-            if os.path.exists(server_file):
-                server_workbook = openpyxl.load_workbook(server_file)
-                server_worksheet = server_workbook.active
-                server_rows = list(server_worksheet.iter_rows(values_only=True))
-                print("File Server P: ", server_rows)
+    mode = 'a' if os.path.exists(filename) else 'w'
+    with open(filename, mode) as file:
+        file.write(data + "\n")
 
-                for row in server_rows[1:]:
-                    if row[2] != 9002:
-                        addr = row[1]
-                        port = row[2]
-                        proxy = xmlrpc.client.ServerProxy(f"http://{addr}:{port}/", allow_none=True)
-                        proxy.write(filename, data, False)
+    if primary:
+        print("Sending data to backup servers")
+        server_file = "Servers.xlsx"
+        if os.path.exists(server_file):
+            server_workbook = openpyxl.load_workbook(server_file)
+            server_worksheet = server_workbook.active
+            server_rows = list(server_worksheet.iter_rows(values_only=True))
 
-            else:
-                return False
-        return True
-    else:
-        file = open(filename, "x")
-        file.write(data)
-        file.close()
-        if primary == True:
-            print("Sending data to backup servers - 2")
-            server_file = "Servers.xlsx"
-            if os.path.exists(server_file):
-                server_workbook = openpyxl.load_workbook(server_file)
-                server_worksheet = server_workbook.active
-                server_rows = list(server_worksheet.iter_rows(values_only=True))
-                for row in server_rows:
-                    if row[3] != 9002:
-                        addr = row[2]
-                        port = row[3]
-                        proxy = xmlrpc.client.ServerProxy(f"http://{addr}:{port}/", allow_none=True)
-                        proxy.write(filename, data, False)
-            else:
-                return False
+            for row in server_rows[1:]:
+                if row[2] != 9002:
+                    addr = row[1]
+                    port = row[2]
+                    proxy = xmlrpc.client.ServerProxy(f"http://{addr}:{port}/", allow_none=True)
+                    proxy.write(filename, data, False)
+        else:
+            return False
         return True
 
 FileServer_B.register_function(write, "write")
