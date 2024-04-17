6fdd97d8b851c3df5678998341634a565e6bbe7c
Jay Sabva <202101224@daiict.ac.in>
2024-04-17 15:48:34 +0530

    Client Update

--- Client/client.py
+++ Client/client.py
@@ -3,15 +3,12 @@ import xmlrpc.client
 def write_file(filename, mode):
     proxy = xmlrpc.client.ServerProxy("http://localhost:9000/", allow_none=True)
     response = proxy.write(filename)
-    print(response)
-    if response[0] == True:
+    if response[0]:
         addr = response[1]
         port = response[2]
-
         proxy1 = xmlrpc.client.ServerProxy(f"http://{addr}:{port}/", allow_none=True)
         data = input("Enter the data to write to the file: ")
-
-        response = proxy1.write(filename, data, True, mode == 'a')
+        response = proxy1.write(filename, data, True, mode == 'a', response[3])  # Include timestamp
         if response:
             unlock = proxy.unlock(filename)
             print(f"Data written to {filename}")
@@ -23,27 +20,39 @@ def write_file(filename, mode):
 def read_file(filename):
     proxy = xmlrpc.client.ServerProxy("http://localhost:9000/", allow_none=True)
     response = proxy.read(filename)
-    print(response)
-    for server in response:
-        addr = server[0]
-        port = server[1]
-
-        proxy1 = xmlrpc.client.ServerProxy(f"http://{addr}:{port}/", allow_none=True)
-        data = proxy1.read(filename)
-        if data != False:
-            print(f"Data in {filename}:")
-            print(data)
-            break
-
-    if len(response) == 0:
+    if response:
+        for server in response:
+            addr = server[0]
+            port = server[1]
+            proxy1 = xmlrpc.client.ServerProxy(f"http://{addr}:{port}/", allow_none=True)
+            data = proxy1.read(filename)
+            if data:
+                print(f"Data in {filename}:")
+                print(data)
+                break
+        else:
+            print(f"Data in {filename} not found.")
+    else:
         print(f"File {filename} not found.")
 
-
 if __name__ == "__main__":
-    filename = input("Enter the filename to write: ")
-    mode = input("Enter 'w' for write or 'a' for append: ")
-    write_file(filename, mode)
+    while True:
+        print("\nSelect an option:")
+        print("1. Write to a file")
+        print("2. Read from a file")
+        print("3. Quit")
 
-#     add functionality for reading the file
-    filename = input("Enter the filename to read: ")
-    read_file(filename)
+        choice = input("Enter your choice (1/2/3): ")
+
+        if choice == '1':
+            filename = input("Enter the filename to write: ")
+            mode = input("Enter 'w' for write or 'a' for append: ")
+            write_file(filename, mode)
+        elif choice == '2':
+            filename = input("Enter the filename to read: ")
+            read_file(filename)
+        elif choice == '3':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
